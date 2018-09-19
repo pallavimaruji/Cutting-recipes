ab6c299ddd3926f918315e8f112c11df1c88dce5
pallavimaruji <36764894+pallavimaruji@users.noreply.github.com>
2018-09-19 17:13:50 +0530

    Add files via upload

---
+++ Cutting recipes/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i;
+        cin>>n;
+        int a[i];
+        for(i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        for(i=0;i<=n;i++)
+        {
+            if(a[i])
+                ;
+        }
+    }
+    cout << "Hello world!" << endl;
+    return 0;
+}
